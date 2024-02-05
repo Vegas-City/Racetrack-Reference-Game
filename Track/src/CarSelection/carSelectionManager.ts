a171d16a49f95c229f31e79a05393891859d0ae6
Karim Machlab <karim@vegascity.org>
2024-02-05 12:06:24 +0000

    oops. conflicts resolved properly

--- Track/src/CarSelection/carSelectionManager.ts
+++ Track/src/CarSelection/carSelectionManager.ts
@@ -9,19 +9,19 @@ export class CarSelectionManager {
     constructor() {
         CarSelectionManager.instance = this
 
-        this.carChoices.push(new CarChoice("models/selection/car1.glb", {
+        this.carChoices.push(new CarChoice(1, "models/selection/car1.glb", {
             position: Vector3.create(3, 0, 10),
             rotation: Quaternion.fromEulerDegrees(0, 180, 0),
             scale: Vector3.create(0.75, 0.75, 0.75)
         }))
 
-        this.carChoices.push(new CarChoice("models/selection/car2.glb", {
+        this.carChoices.push(new CarChoice(2, "models/selection/car2.glb", {
             position: Vector3.create(6, 0, 10),
             rotation: Quaternion.fromEulerDegrees(0, 180, 0),
             scale: Vector3.create(0.75, 0.75, 0.75)
         }))
 
-        this.carChoices.push(new CarChoice("models/selection/car3.glb", {
+        this.carChoices.push(new CarChoice(3, "models/selection/car3.glb", {
             position: Vector3.create(9, 0, 10),
             rotation: Quaternion.fromEulerDegrees(0, 180, 0),
             scale: Vector3.create(0.75, 0.75, 0.75)
