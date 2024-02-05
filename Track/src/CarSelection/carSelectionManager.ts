40aaea68fc731db173edf4861b5e9e21718c352d
Karim Machlab <karim@vegascity.org>
2024-02-05 12:01:34 +0000

    1st person position change

--- Track/src/CarSelection/carSelectionManager.ts
+++ Track/src/CarSelection/carSelectionManager.ts
@@ -3,38 +3,38 @@ import { CarChoice } from "./carChoice";
 import { Quaternion, Vector3 } from "@dcl/ecs-math";
 
 export class CarSelectionManager {
-    carChoices:CarChoice[] = []
-    static instance:CarSelectionManager
+    carChoices: CarChoice[] = []
+    static instance: CarSelectionManager
 
-    constructor(){
+    constructor() {
         CarSelectionManager.instance = this
 
-        this.carChoices.push(new CarChoice("models/selection/car1.glb",{
-            position: Vector3.create(3,0,10),
-            rotation: Quaternion.fromEulerDegrees(0,180,0),
-            scale: Vector3.create(0.75,0.75,0.75)
+        this.carChoices.push(new CarChoice("models/selection/car1.glb", {
+            position: Vector3.create(3, 0, 10),
+            rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+            scale: Vector3.create(0.75, 0.75, 0.75)
         }))
 
-        this.carChoices.push(new CarChoice("models/selection/car2.glb",{
-            position: Vector3.create(6,0,10),
-            rotation: Quaternion.fromEulerDegrees(0,180,0),
-            scale: Vector3.create(0.75,0.75,0.75)
+        this.carChoices.push(new CarChoice("models/selection/car2.glb", {
+            position: Vector3.create(6, 0, 10),
+            rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+            scale: Vector3.create(0.75, 0.75, 0.75)
         }))
 
-        this.carChoices.push(new CarChoice("models/selection/car3.glb",{
-            position: Vector3.create(9,0,10),
-            rotation: Quaternion.fromEulerDegrees(0,180,0), 
-            scale: Vector3.create(0.75,0.75,0.75)
+        this.carChoices.push(new CarChoice("models/selection/car3.glb", {
+            position: Vector3.create(9, 0, 10),
+            rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+            scale: Vector3.create(0.75, 0.75, 0.75)
         }))
     }
 
-    static show(){
+    static show() {
         CarSelectionManager.instance.carChoices.forEach(car => {
             car.show()
         });
     }
 
-    static hide(){
+    static hide() {
         CarSelectionManager.instance.carChoices.forEach(car => {
             car.hide()
         });
