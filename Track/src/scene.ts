d8107bf7d7fd2d75cabb5bfe3c849ef8e8fe21f2
Bobby H <bobbyhiom@googlemail.com>
2024-02-07 15:45:33 +0000

    Ghost server WIP

--- Track/src/scene.ts
+++ Track/src/scene.ts
@@ -10,6 +10,7 @@ import * as trackConfig1 from "../data/track_01.json"
 import * as trackConfig2 from "../data/track_02.json"
 import * as trackConfig3 from "../data/track_03.json"
 import { Entity, GltfContainer, Transform, engine } from '@dcl/sdk/ecs'
+import { GhostRecorder } from '@vegascity/racetrack/src/ghostCar'
 
 export class Scene {
 
@@ -36,6 +37,11 @@ export class Scene {
                     checkpoint: Lap.checkpointIndex + (Lap.checkpoints.length * (Lap.lapsCompleted * 2)),
                     time: Math.round(Lap.timeElapsed * 1000)
                 })
+
+                // Send the ghost to the server
+                if(GhostRecorder.instance!=null){
+                    ServerComms.recordGhostData(GhostRecorder.instance.getGhostData())
+                }
             },
             () => {
                 ServerComms.recordAttempt({
