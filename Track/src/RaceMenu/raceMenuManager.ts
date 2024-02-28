64522db83cb468245026fdf561bcb03bad147180
Karim Machlab <karim@vegascity.org>
2024-02-28 12:50:30 +0000

    practice track 1 has its own cup

--- Track/src/RaceMenu/raceMenuManager.ts
+++ Track/src/RaceMenu/raceMenuManager.ts
@@ -150,6 +150,7 @@ export class RaceMenuManager {
                 this.trackButton1.select()
                 Transform.getMutable(this.practiceIcon).scale = Vector3.One()
                 Transform.getMutable(this.competitionIcon).scale = Vector3.Zero()
+                RaceMenuManager.update()
             }).bind(this)
         })
 
@@ -171,6 +172,7 @@ export class RaceMenuManager {
                 this.trackButton4.show()
                 Transform.getMutable(this.practiceIcon).scale = Vector3.Zero()
                 Transform.getMutable(this.competitionIcon).scale = Vector3.One()
+                RaceMenuManager.update()
             }).bind(this),
             iconOffset: Vector3.create(-0.35, 0, 0),
             iconScale: Vector3.create(0.8, 0.8, 0.8)
@@ -453,7 +455,7 @@ export class RaceMenuManager {
 
             track.carPbsPerTrack.forEach(carPb => {
                 if (carPb.car == selectedCarGuid && carPb.PB > 0 && carPb.PB < track.targetTimeToUnlockNextTrack) {
-                    if (track.guid == "6a0a3950-bcfb-4eb4-9166-61edc233b82b") {
+                    if (track.guid == "6a0a3950-bcfb-4eb4-9166-61edc233b82b" && RaceMenuManager.instance.competitionButton.selected) {
                         RaceMenuManager.instance.trackButton1.setQualified()
                     }
                     else if (track.guid == "17e75c78-7f17-4b7f-8a13-9d1832ec1231") {
@@ -470,8 +472,11 @@ export class RaceMenuManager {
         })
 
         if (ServerComms.player.practiceCompleted) {
-            console.log(ServerComms.player.practiceCompleted)
             RaceMenuManager.instance.competitionButton.unlock()
+
+            if (RaceMenuManager.instance.practiceButton.selected) {
+                RaceMenuManager.instance.trackButton1.setQualified()
+            }
         }
 
         if ((RaceMenuManager.instance.trackButton2.selected && RaceMenuManager.instance.trackButton2.locked)
