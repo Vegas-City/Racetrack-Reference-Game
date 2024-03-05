c790e6f03bfabd36a6d3424a5cbeffcc9361581b
Bobby H <bobby@vegascity.org>
2024-03-05 11:48:14 +0000

    Fixed

--- Track/src/scene.ts
+++ Track/src/scene.ts
@@ -35,7 +35,7 @@ export class Scene {
     }
 
     static LoadScene(): void {
-        setup(movePlayerTo, triggerSceneEmote)
+        setup(movePlayerTo, triggerSceneEmote) 
 
         new InputManager()
 
@@ -130,6 +130,9 @@ export class Scene {
 
                     if (TrackManager.isPractice) {
                         if (Math.round(lap.timeElapsed) < 50) {
+                            if(!ServerComms.player.practiceCompleted){
+                                EventUIImage.triggerEvent(EventUIEnum.competitionUnlockEvent)
+                            }
                             ServerComms.completePractice()
                         }
                     }
