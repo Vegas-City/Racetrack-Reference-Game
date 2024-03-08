b06d5a5cdfcc4bb5a2eb9355757068e42046ad29
Karim Machlab <karim@vegascity.org>
2024-03-08 14:36:30 +0000

    fixed times

--- Track/src/party/partyManager.ts
+++ Track/src/party/partyManager.ts
@@ -12,18 +12,18 @@ export class PartyManager {
         this.leaderboard = new LeaderboardUI(Vector3.create(39, 10, 98), Quaternion.fromEulerDegrees(0, -75, 0), Vector3.create(0.3, 0.3, 0.3), 6, 2.05, false)
         this.leaderboard.hide()
 
-        // Party starts in... 7:30-8:00
+        // Party starts in... 7:30-8:00 pm
         new Countdown3d(new Date('2024-03-17T19:30:00'), 30 * 60, Vector3.create(87.8, 12.1, 103.1), Quaternion.fromEulerDegrees(0, 262.5, 0), Vector3.create(2, 2, 2))
         new Countdown3d(new Date('2024-03-17T19:30:00'), 30 * 60, Vector3.create(79.5, 12.1, 76.6), Quaternion.fromEulerDegrees(0, -47, 0), Vector3.create(2, 2, 2))
         
-        // Racing ends in... 8:00-8:27
+        // Racing ends in... 8:00-8:27 pm
         new Countdown3d(new Date('2024-03-17T20:00:00'), 27 * 60, Vector3.create(87.8, 12.1, 103.1), Quaternion.fromEulerDegrees(0, 262.5, 0), Vector3.create(2, 2, 2))
         new Countdown3d(new Date('2024-03-17T20:00:00'), 27 * 60, Vector3.create(79.5, 12.1, 76.6), Quaternion.fromEulerDegrees(0, -47, 0), Vector3.create(2, 2, 2))
 
         ScheduleManager.instance.registerSchedule(
             new Schedule(
-                Date.UTC(2024, 2, 17, 8, 0),
-                Date.UTC(2024, 2, 17, 8, 30),
+                Date.UTC(2024, 2, 17, 20, 0),
+                Date.UTC(2024, 2, 17, 20, 30),
                 () => {
                     this.dj = new DJ()
                 },
@@ -37,8 +37,8 @@ export class PartyManager {
 
         ScheduleManager.instance.registerSchedule(
             new Schedule(
-                Date.UTC(2024, 2, 17, 8, 0),
-                Date.UTC(2024, 2, 17, 8, 45),
+                Date.UTC(2024, 2, 17, 20, 0),
+                Date.UTC(2024, 2, 17, 20, 45),
                 () => {
                     this.leaderboard.show()
                 },
