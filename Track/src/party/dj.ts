a493dce68cd1dd0dd2f9884a2f63438a1d4f7c90
Bobby H <bobbyhiom@googlemail.com>
2024-03-12 09:31:14 +0000

    Updated DJ

--- Track/src/party/dj.ts
+++ Track/src/party/dj.ts
@@ -5,7 +5,7 @@ export class DJ {
     entity:Entity
     constructor(){
         this.entity = engine.addEntity()
-        Transform.create(this.entity, {position:Vector3.create(75,4,85), rotation:Quaternion.fromEulerDegrees(0,-90,0),scale:Vector3.create(1.3,1.3,1.3)})
+        Transform.create(this.entity, {position:Vector3.create(75,4,85), rotation:Quaternion.fromEulerDegrees(0,-90,0),scale:Vector3.create(1,1,1)})
         GltfContainer.create(this.entity, {src:"models/npcs/dj.glb"})
     }
 
