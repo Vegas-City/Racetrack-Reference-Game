41fc7c8eb640fe8061f44caa8c0802592dd59b9e
Karim Machlab <karim@vegascity.org>
2024-03-13 16:13:54 +0000

    reverted to using package version 1.0.104

--- Track/src/RaceMenu/carChoice.ts
+++ Track/src/RaceMenu/carChoice.ts
@@ -34,8 +34,9 @@ export class CarChoice {
         // Load attributes from the JSON
         let carStats = carConfiguration.cars[this.carIndex]
         ServerComms.currentCar = carStats.guid
-        CarFactory.create(Vector3.create(8.45, 1.4, 23.7), 90,
+        CarFactory.create(
             {
+                dashboardGLB: "",
                 mass: carStats.attributes.mass,
                 accelerationF: carStats.attributes.accelerationF,
                 accelerationB: carStats.attributes.accelerationB,
@@ -70,7 +71,7 @@ export class CarChoice {
                 firstPersonCagePosition: this.extractVectorFromString(carStats.positions.firstPersonCagePosition),
                 thirdPersonCagePosition: this.extractVectorFromString(carStats.positions.thirdPersonCagePosition),
                 carIcon: carStats.images.carIcon
-            })
+            }, Vector3.create(8.45, 1.4, 23.7), 90)
     }
 
     extractVectorFromString(_data: string): Vector3 {
