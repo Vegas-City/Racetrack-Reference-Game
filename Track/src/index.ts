e7be6ab3b709bfe4fd18b088aad10eb0ac923a70
Bobby H <bobbyhiom@googlemail.com>
2024-03-14 12:08:40 +0000

    Revert "Remove password protection. Rely on time gating"
    This reverts commit 3e49fdd2fbfa743a06ed8e5a612d51f5b3151cd0.

--- Track/src/index.ts
+++ Track/src/index.ts
@@ -12,33 +12,86 @@ import { PartyManager } from "./party/partyManager";
 import { IntervalLogger } from "@vegascity/vegas-city-logger/dist/logger/IntervalLogger";
 import { AudioManager } from "./audio/audioManager";
 
+const passwordProtected: boolean = true
+const password: string = "letsgo"
+const passwordDev: string = "letsgodev"
 
 export function main() {
   setupUi()
-
+ 
   // wait for the realm and user data to be available
   Helper.init(() => {
 
-    new IntervalLogger("RACETRACK", ecs.engine, ecs.Transform, 10)
+    new IntervalLogger("RACETRACK",ecs.engine,ecs.Transform,10)
 
     UserData.getUserData(() => {
       executeTask(async () => {
         const { realmInfo } = await getRealm({})
         if (realmInfo != undefined) {
           console.log(`You are in the realm: `, realmInfo.realmName)
-          Scene.LoadBuildings()
-          utils.timers.setTimeout(() => {
-            Scene.LoadScene()
-            new PartyManager()
-            if (realmInfo.isPreview) {
+          if (realmInfo.isPreview) {
+            Scene.LoadBuildings()
+            utils.timers.setTimeout(() => {
+              Scene.LoadScene()
+              new NPCManager()
+              new PartyManager()
               DebugUI.debugUIShow = true
-            }
-          }, 1500)
-          utils.timers.setTimeout(()=>{
-            AudioManager.playMusic(4) // background music
-          }, 3000)
+            }, 1500)
+          }
+          else {
+            Scene.LoadBuildings()
+            utils.timers.setTimeout(() => {
+              Scene.LoadScene()
+              if (passwordProtected) {
+                showPrompt()
+              }
+              else {
+                new NPCManager()
+                new PartyManager()
+              }
+            }, 1500)
+          }
         }
       })
     })
   })
+
+  function showPrompt() {
+    const prompt = ui.createComponent(ui.FillInPrompt, {
+      title: 'Enter password',
+      onAccept: (value: string) => {
+        if (value.toLocaleLowerCase() == password) {
+          prompt.hide()
+        } else if (value.toLocaleLowerCase() == passwordDev) {
+          prompt.hide()
+          utils.timers.setTimeout(function () {
+            new NPCManager()
+            new PartyManager()
+            DebugUI.debugUIShow = true
+          }, 1000)
+        }
+      }
+    })
+
+    prompt.inputElement.onChange = (value: string) => {
+      if (value.toLocaleLowerCase() == password) {
+        prompt.hide()
+        utils.timers.setTimeout(function () {
+          new NPCManager()
+          new PartyManager()
+          prompt.hide()
+        }, 1000)
+      } else if (value.toLocaleLowerCase() == passwordDev) {
+        prompt.hide()
+        utils.timers.setTimeout(function () {
+          new NPCManager()
+          new PartyManager()
+          prompt.hide()
+          DebugUI.debugUIShow = true
+        }, 1000)
+      }
+    }
+
+    prompt.show()
+  }
 }
