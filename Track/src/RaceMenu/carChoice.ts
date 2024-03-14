7f7d72fb970243b7fb26780b2657d203f0ef01ab
Karim Machlab <karim@vegascity.org>
2024-03-14 01:07:02 +0000

    using new npm package

--- Track/src/RaceMenu/carChoice.ts
+++ Track/src/RaceMenu/carChoice.ts
@@ -34,7 +34,7 @@ export class CarChoice {
         // Load attributes from the JSON
         let carStats = carConfiguration.cars[this.carIndex]
         ServerComms.currentCar = carStats.guid
-        CarFactory.create(Vector3.create(8.45, 1.4, 23.7), 90,
+        CarFactory.create(
             {
                 mass: carStats.attributes.mass,
                 accelerationF: carStats.attributes.accelerationF,
@@ -70,7 +70,7 @@ export class CarChoice {
                 firstPersonCagePosition: this.extractVectorFromString(carStats.positions.firstPersonCagePosition),
                 thirdPersonCagePosition: this.extractVectorFromString(carStats.positions.thirdPersonCagePosition),
                 carIcon: carStats.images.carIcon
-            })
+            }, Vector3.create(8.45, 1.4, 23.7), 90)
     }
 
     extractVectorFromString(_data: string): Vector3 {
