3f1bf0aea64f0013659d0ee10b46581905c51bbc
Karim Machlab <karim@vegascity.org>
2024-03-16 06:38:08 +0000

    screen and podium NPC fixes

--- Track/src/NPCs/NPCManager.ts
+++ Track/src/NPCs/NPCManager.ts
@@ -3,13 +3,13 @@ import { EntranceNPC } from "./entranceNPC";
 import { GuideNPC } from "./guideNPC";
 import { PitstopNPC } from "./pitstopNPC";
 
-export class NPCManager{
-    constructor(){
+export class NPCManager {
+    constructor() {
         try {
-        new GuideNPC()
-        new EntranceNPC()
-        new PitstopNPC()
-        } catch (error){
+            new GuideNPC()
+            new EntranceNPC()
+            new PitstopNPC()
+        } catch (error) {
             console.log("NPC creation error: " + error)
         }
     }
